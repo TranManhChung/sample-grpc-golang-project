3bf0ca306e440fbc64f399cd4cb3802dfbd24f3c
Tran Manh Chung <chungtm>
2019-12-03 15:26:38 +0700

    add worker

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	sample "github.com/TranManhChung/sample-grpc-golang-project/grpc-gen"
 
@@ -15,15 +20,27 @@ const (
 	port = ":56789"
 )
 
+var queueJob chan string
+
 type server struct{}
 
 // SampleAPI ...
 func (s *server) SampleAPI(ctx context.Context, req *sample.SampleReq) (*sample.SampleRes, error) {
-
+	time.Sleep(1 * time.Second)
+	queueJob <- req.Mess
 	return &sample.SampleRes{Data: &sample.SampleRes_Data{Data: req.Mess}}, nil
 }
 
 func main() {
+	queueJob = make(chan string, 10)
+	for i := 0; i < 8; i++ {
+		go func() {
+			for j := range queueJob {
+				fmt.Println(j)
+			}
+		}()
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -32,7 +49,20 @@ func main() {
 	sample.RegisterSampleServiceServer(s, &server{})
 
 	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+		fmt.Println("Server now listening")
+	}()
+	go func() {
+		<-sigs
+		done <- true
+	}()
+	fmt.Println("Ctrl-C to interrupt...")
+	<-done
 }
--- test/integration/sample_test.go
+++ test/integration/sample_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	sample "github.com/TranManhChung/sample-grpc-golang-project/grpc-gen"
@@ -9,8 +10,10 @@ import (
 )
 
 func Test_Sample(t *testing.T) {
-	data := "hello"
-	r, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
-	assert.Nil(t, e)
-	assert.Equal(t, r.Data.Data, data)
+	for i := 0; i < 100; i++ {
+		data := strconv.Itoa(i)
+		r, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
+		assert.Nil(t, e)
+		assert.Equal(t, r.Data.Data, data)
+	}
 }
