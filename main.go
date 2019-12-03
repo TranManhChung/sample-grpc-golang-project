9d5bee29860973cc60902244c2318642db226b57
Tran Manh Chung <chungtm>
2019-12-03 17:05:26 +0700

    add solution

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ import (
 )
 
 const (
-	port = ":56789"
+	port      = ":56789"
+	maxWorker = 8
 )
 
 var queueJob chan string
@@ -26,19 +27,33 @@ type server struct{}
 
 // SampleAPI ...
 func (s *server) SampleAPI(ctx context.Context, req *sample.SampleReq) (*sample.SampleRes, error) {
-	time.Sleep(1 * time.Second)
 	queueJob <- req.Mess
 	return &sample.SampleRes{Data: &sample.SampleRes_Data{Data: req.Mess}}, nil
 }
 
 func main() {
-	queueJob = make(chan string, 10)
-	for i := 0; i < 8; i++ {
-		go func() {
-			for j := range queueJob {
-				fmt.Println(j)
+	done := make(chan bool, 1)
+	var stopSig []chan bool
+
+	queueJob = make(chan string, 100)
+	var job string
+	for i := 0; i < maxWorker; i++ {
+		a := make(chan bool, 1)
+		stopSig = append(stopSig, a)
+		go func(i int) {
+			for {
+				time.Sleep(1 * time.Second)
+				select {
+				case job = <-queueJob:
+					fmt.Println(job)
+				case stop := <-done:
+					if stop == false && len(queueJob) == 0 {
+						stopSig[i] <- true
+						return
+					}
+				}
 			}
-		}()
+		}(i)
 	}
 
 	lis, err := net.Listen("tcp", port)
@@ -47,22 +62,22 @@ func main() {
 	}
 	s := grpc.NewServer()
 	sample.RegisterSampleServiceServer(s, &server{})
-
 	reflection.Register(s)
-
 	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
-		fmt.Println("Server now listening")
 	}()
 	go func() {
 		<-sigs
-		done <- true
+		s.Stop()
+		close(done)
 	}()
 	fmt.Println("Ctrl-C to interrupt...")
-	<-done
+	for i := 0; i < maxWorker; i++ {
+		<-stopSig[i]
+	}
+
 }
--- test/integration/sample_test.go
+++ test/integration/sample_test.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"context"
 	"strconv"
+	"sync"
 	"testing"
 
 	sample "github.com/TranManhChung/sample-grpc-golang-project/grpc-gen"
@@ -10,10 +11,16 @@ import (
 )
 
 func Test_Sample(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(100)
 	for i := 0; i < 100; i++ {
-		data := strconv.Itoa(i)
-		r, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
-		assert.Nil(t, e)
-		assert.Equal(t, r.Data.Data, data)
+		go func(i int) {
+			defer wg.Done()
+			data := strconv.Itoa(i)
+			r, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
+			assert.Nil(t, e)
+			assert.Equal(t, r.Data.Data, data)
+		}(i)
 	}
+	wg.Wait()
 }
