5601ce970c479be78f622e96020fe4448110d10f
Tran Manh Chung <chungtm>
2020-11-03 20:48:50 +0700

    add error handler

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 	sample "github.com/TranManhChung/sample-grpc-golang-project/grpc-gen"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -19,7 +22,24 @@ type server struct{}
 
 // SampleAPI ...
 func (s *server) SampleAPI(ctx context.Context, req *sample.SampleReq) (*sample.SampleRes, error) {
-
+	if req.GetMess() == "" {
+		errInfo := &errdetails.ErrorInfo{
+			Type:   "INVALID_PARAM",
+			Domain: "sample.grpc",
+			Metadata: map[string]string{
+				"h5": "h5 token is empty",
+			},
+		}
+		clientMsg := &errdetails.LocalizedMessage{
+			Locale:  "vietnamese",
+			Message: "Thông tin không hợp lệ",
+		}
+		stt, _ := status.New(codes.InvalidArgument, codes.InvalidArgument.String()).WithDetails(
+			errInfo,
+			clientMsg,
+		)
+		return nil, stt.Err()
+	}
 	return &sample.SampleRes{Data: &sample.SampleRes_Data{Data: req.Mess}}, nil
 }
 
--- test/integration/sample_test.go
+++ test/integration/sample_test.go
@@ -2,10 +2,14 @@ package integration
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	sample "github.com/TranManhChung/sample-grpc-golang-project/grpc-gen"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/status"
 )
 
 func Test_Sample(t *testing.T) {
@@ -13,4 +17,22 @@ func Test_Sample(t *testing.T) {
 	r, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
 	assert.Nil(t, e)
 	assert.Equal(t, r.Data.Data, data)
+}
+
+func Test_ParseStatus(t *testing.T) {
+	data := ""
+	_, e := client.SampleAPI(context.Background(), &sample.SampleReq{Mess: data})
+	stt, _ := status.FromError(e)
+
+	out, _ := json.Marshal(e)
+	fmt.Println(string(out))
+
+	fmt.Println("status_code : ", stt.Code())
+	fmt.Println("status_message : ", stt.Message())
+	fmt.Println("status_detail : ", stt.Details())
+	fmt.Println("status_data : ", stt.Proto())
+
+	for i, detail := range stt.Details() {
+		fmt.Println("detail : ", i, " / ", detail.(*errdetails.ErrorInfo))
+	}
 }
